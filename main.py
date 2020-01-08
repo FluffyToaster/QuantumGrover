dbf2ef79cdbe895e86a2b95c5a5d1c76c95e7fe5
FluffyToaster <dev.fluffytoaster@gmail.com>
2020-01-08 12:18:03 +0100

    Add initial Python setup

---
+++ main.py
@@ -0,0 +1,92 @@
+from quantuminspire.credentials import enable_account
+from quantuminspire.api import QuantumInspireAPI
+import matplotlib.pyplot as plt
+import math
+
+QUBIT_COUNT = 2
+SHOT_COUNT = 64
+
+
+def int_to_bits(int_str):
+    """
+    Convert a number (possibly in string form) to a readable bit format.
+    For example, the result '11', which means both qubits were measured as 1, is returned by the API as "3".
+    This converts that output to the readable version.
+
+    Args:
+        int_str: A string or integer in base 10 that represents the measurement outcome.
+
+    Returns: A string of 0's and 1's
+    """
+    return str(bin(int(int_str)))[2:].zfill(QUBIT_COUNT)
+
+
+def interpret_results(result_dict):
+    """
+    Parse the result dictionary given by the API into a readable format, and plot it.
+
+    Args:
+        result_dict: The dictionary given by qi.execute_qasm
+
+    Returns: Parsed result
+    """
+    num_of_measurements = 2 ** QUBIT_COUNT
+    ordered_bars = [None for _ in range(num_of_measurements)]
+
+    for i in range(num_of_measurements):
+        # find result in dictionary and add to list of bars
+        try:
+            bar = result_dict["histogram"][str(i)]
+        except KeyError:
+            bar = 0
+
+        # generate corresponding binary name (so "11" instead of "3")
+        name = int_to_bits(i)
+
+        # add to list of bars
+        ordered_bars[i] = (name, bar)
+
+    for b in ordered_bars:
+        plt.bar(*b)
+    plt.show()
+    return ordered_bars
+
+
+enable_account("58957ea5a48a801eb5af6adcae7776126c122c9d")
+qi = QuantumInspireAPI()
+
+print("Logged in to QI account")
+
+qasm_header = """
+
+version 1.0
+
+qubits {}
+
+""".format(QUBIT_COUNT)
+
+qasm_program_file = open("program.qasm", "r")
+qasm_program = qasm_program_file.read(-1)
+
+replacements = [
+    ("PI1", math.pi),
+    ("PI2", math.pi / 2),
+    ("PI3", math.pi / 3),
+    ("PI4", math.pi / 4),
+    ("PI6", math.pi / 6),
+]
+
+for r_text, r_repl in replacements:
+    print("replacing {} by {}".format(r_text, r_repl))
+    qasm_program = qasm_program.replace(r_text, str(r_repl))
+
+print(qasm_program)
+
+qasm_complete = qasm_header + qasm_program
+
+backend = qi.get_backend_type_by_name('QX single-node simulator')
+
+print("Executing QASM code ({} qubits, {} shots)".format(QUBIT_COUNT, SHOT_COUNT))
+result = qi.execute_qasm(qasm_complete, backend_type=backend, number_of_shots=SHOT_COUNT)
+print("Execution complete, printing and plotting results")
+print(interpret_results(result))
---
+++ program.qasm
@@ -0,0 +1,14 @@
+# Single gates
+# Pauli gates: X, Y, Z
+# Hadamard H
+# Rotation: Rx, Ry, Rz. Ex: Rx q[0], 3.14/4
+
+# Two qubit gates
+# CNOT
+
+# Measurement
+# Measure_x, Measure_y, Measure_z
+
+# Start of program
+
+Ry q[0], PI2
